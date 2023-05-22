73a43b1c280189654b8eef8888489f692ef785ad
吴雨洙 <1547344067@qq.com>
2023-05-22 23:25:50 +0800

    Create FinalMain.c

---
+++ FinalMain.c
@@ -0,0 +1,70 @@
+#include <Windows.h>
+#include <winuser.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "graphics.h"
+#include "extgraph.h"
+#include "imgui.h"
+
+#include "UI.h"
+#include"File.h"
+#include"Event.h"
+#include"Write.h"
+#include"Class.h"
+
+#define MAX_LINE 100
+#define MAX_ROWS 50
+
+extern char text[MAX_ROWS][MAX_LINE + 1];
+extern char notename[50][100];
+extern char noteclass[50];
+extern int allnotes;
+
+typedef struct  myNote {
+	char mytitle[40];  //笔记名称 
+	char myclass;  //笔记类别 
+	struct myNote* next;
+}Note;
+extern Note* head, * tail;
+int line_length_count;
+
+typedef struct line {
+	char linetext[100];
+	struct line* next;
+}line;
+
+extern double winheight, winwidth; 
+extern int flag;
+extern int current_line;
+extern int current_line_length;
+extern int line_count;
+extern int ifopen;
+extern int ifsave;
+extern double mouse_x, mouse_y; 
+extern int reserveOneChar;
+extern int classflag; 
+extern int ifpress;  
+extern double text_x, text_y, text_height;
+extern char lei;
+int allclasses = 6; 
+char yclass;
+
+void Main()
+{
+	ReadUserData();
+	SetWindowTitle("笔记管理系统");
+	SetWindowSize(10, 10);
+	winheight = 10.0;
+	winwidth = 10.0;
+	InitGraphics();
+	text_height = GetFontHeight() * 1.5;
+	text_x = 0.2;
+	text_y = GetWindowHeight() / 8 * 7;
+	registerCharEvent(myCharEvent);
+	registerKeyboardEvent(myKeyboardEvent);
+	registerMouseEvent(myMouseEvent);
+
+	DrawHeadArea();
+	DrawNotebooks();
+}
