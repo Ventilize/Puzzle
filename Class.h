49f843f33682c961a39e62ca85c7c702e668e4b9
吴雨洙 <1547344067@qq.com>
2023-05-22 14:36:49 +0800

    Create Class.h

---
+++ Class.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _Class_h
+#define _Class_h
+/*支持点击查看文件*/
+void class_display();
+/*画出6个类别*/
+void drawClass();
+/*进入某一类别*/
+void displayclassAll();
+/*查找笔记*/
+int search(char* name);
+/*创建链表*/
+void LinkList();
+/*笔记删除*/
+void Delete(char* name);
+
+#endif
